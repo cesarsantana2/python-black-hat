ab07dd16180fa20c47875051cc91c115a8e11a6f
Cesar Santana <cesar.santana@nubank.com.br>
2022-03-05 18:15:34 -0300

    Adding a tcpserver

--- network/tcpclient.py
+++ network/tcpclient.py
@@ -7,10 +7,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-cliend.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\nHost: google.com\n\n")
 
 response = client.recv(4096)
 
-print response
+print(response)
 
 
