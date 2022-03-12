15225428f17d213e585e565e522a1653594788fe
Cesar Santana <cjsseng@gmail.com>
2022-03-12 15:52:32 -0300

    Fixing enconding issues

--- network/nc.py
+++ network/nc.py
@@ -14,77 +14,14 @@ upload_destination = ""
 port = 0
 
 
-def usage():
-    print("nc\n")
-    print("Usage: nc.py -t target_host -p port\n")
-    print("-l --listen                  - listen on [host]:[port] for incoming connections\n")
-    print("-e --execyte=file_to_run     - execute the give file upon receiving a connection\n")
-    print("-c --command                 - initialaze a command shell\n")
-    print("-u --upload=destination      - upon receiving connection upload a file and write to [destination]\n\n")
-    print("Examples:\n")
-    print("nc.py -t 192.168.0.1 -p 5555 -l -c\n")
-    print("nc.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe\n")
-    print("nc.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\n")
-    print("echo 'ABCDEFGHI' | ./nc.py -t 192.168.0.1 -p 135")
-    sys.exit(0)
-
-
-def client_sender(buffer):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run_command(cmd):
+    cmd = cmd.rstrip()
 
     try:
-        client.connect((target,port))
-
-        if len(buffer):
-            client.send(buffer)
-
-        while True:
-            recv_len = 1
-            response = ""
-
-            while recv_len:
-                data     = client.recv(4096)
-                recv_len = len(data)
-                response+= data
-
-                if recv_len < 4096:
-                    break
-            
-            print(response,)
-
-            buffer = input("")
-            buffer+= "\n"
-
-            client.send(buffer)
-
-    except:
-        print("[*] Exception! Exiting.")
-        client.close()
-
-
-def server_loop():
-    global target
-
-    if not len(target):
-        target = "0.0.0.0"
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((target,port))
-    server.listen(5)
-
-    while True:
-        client_socket, add = server.accept()
-        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
-        client_thread.start()
-
-
-def run_command(command):
-    command = command.rstrip()
-
-    try:
-        output = subprocess.check_output(command,stderr=subprocess.STDOUT, shell=True)
-    except:
-        output = "Failed to execute command. \r\n"
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT,
+                                         shell=True)
+    except subprocess.CalledProcessError as e:
+        output = e.output
 
     return output
 
@@ -94,8 +31,11 @@ def client_handler(client_socket):
     global execute
     global command
 
+
     if len(upload_destination):
+
         file_buffer = ""
+
         while True:
             data = client_socket.recv(1024)
 
@@ -104,32 +44,111 @@ def client_handler(client_socket):
             else:
                 file_buffer += data
 
-        
         try:
             file_descriptor = open(upload_destination, "wb")
-            file_descriptor.write(file_buffer)
+            file_descriptor.write(file_buffer.encode('utf-8'))
             file_descriptor.close()
-            client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
-        except:
-            client_socket.send("Failed to save file to %s\r\n" % upload_destination)
 
-    
+            client_socket.send(
+                "Successfully saved file to %s\r\n" % upload_destination)
+        except OSError:
+            client_socket.send(
+                "Failed to save file to %s\r\n" % upload_destination)
+
     if len(execute):
         output = run_command(execute)
+
         client_socket.send(output)
 
-
     if command:
-        while True:
-            client_socket.send(b"<nc:# ")
 
-            cmd_buffer = b""
+        while True:
+            client_socket.send("<nc:#> ".encode('utf-8'))
+
+            cmd_buffer = b''
             while b"\n" not in cmd_buffer:
                 cmd_buffer += client_socket.recv(1024)
 
             response = run_command(cmd_buffer)
 
             client_socket.send(response)
+
+
+def server_loop():
+    global target
+    global port
+
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        client_thread = threading.Thread(target=client_handler,
+                                         args=(client_socket,))
+        client_thread.start()
+
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((target, port))
+
+        if len(buffer):
+            client.send(buffer.encode('utf-8'))
+
+        while True:
+            recv_len = 1
+            response = b''
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096:
+                    break
+
+            print(response.decode('utf-8'), end=' ')
+
+            buffer = input("")
+            buffer += "\n"
+
+            client.send(buffer.encode('utf-8'))
+
+    except socket.error as exc:
+        print("[*] Exception! Exiting.")
+
+        client.close()
+
+
+def usage():
+    print("Netcat Replacement")
+    print()
+    print("Usage: nc.py -t target_host -p port")
+    print(
+        "-l --listen                - listen on [host]:[port] for incoming "
+        "connections")
+    print(
+        "-e --execute=file_to_run   - execute the given file upon receiving "
+        "a connection")
+    print("-c --command               - initialize a command shell")
+    print(
+        "-u --upload=destination    - upon receiving connection upload a file "
+        "and write to [destination]")
+    print()
+    print()
+    print("Examples: ")
+    print("nc.py -t 192.168.0.1 -p 5555 -l -c")
+    print("nc.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe")
+    print("nc.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"")
+    print("echo 'ABCDEFGHI' | ./nc.py -t 192.168.11.12 -p 135")
+    sys.exit(0)
 
 
 def main():
@@ -140,40 +159,39 @@ def main():
     global upload_destination
     global target
 
-
     if not len(sys.argv[1:]):
         usage()
 
-
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:", ["help","listen","execute","target","port","command","upload"])
+        opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu:",
+                                   ["help", "listen", "execute", "target",
+                                    "port", "command", "upload"])
+        for o, a in opts:
+            if o in ("-h", "--help"):
+                usage()
+            elif o in ("-l", "--listen"):
+                listen = True
+            elif o in ("-e", "--execute"):
+                execute = a
+            elif o in ("-c", "--commandshell"):
+                command = True
+            elif o in ("-u", "--upload"):
+                upload_destination = a
+            elif o in ("-t", "--target"):
+                target = a
+            elif o in ("-p", "--port"):
+                port = int(a)
+            else:
+                assert False, "Unhandled Option"
+
     except getopt.GetoptError as err:
         print(str(err))
-        print("\n")
         usage()
 
-
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            usage()
-        elif o in ("-l", "--listen"):
-            listen = True
-        elif o in ("-e", "--execute"):
-            execute = a
-        elif o in ("-c", "--cammanda"):
-            command = True
-        elif o in ("-u","--upload"):
-            upload_destination = a
-        elif o in ("-t", "--target"):
-            target = a
-        elif o in ("-p", "--port"):
-            port = int(a)
-        else:
-            assert False, "Unhandled Option"
-
-
     if not listen and len(target) and port > 0:
+
         buffer = sys.stdin.read()
+
         client_sender(buffer)
 
     if listen:
@@ -181,3 +199,4 @@ def main():
 
 
 main()
+
