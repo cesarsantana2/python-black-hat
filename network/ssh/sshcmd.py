314c3645b28a4665f1084e571fefe9bdd64022f0
Cesar Santana <cjsseng@gmail.com>
2022-03-19 19:37:27 -0300

    finishing paramiko

--- network/ssh/sshcmd.py
+++ network/ssh/sshcmd.py
@@ -7,3 +7,13 @@ def ssh_command(ip, user, passwd, command):
 
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, username=user, password=passwd)
+    
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024))
+
+    return
+
+ssh_command("192.168.100.131", "justin", "lovesthepython", "id")
