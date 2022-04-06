f39e2ad4bfc1dbee1ac44aaff3de41572800283d
Cesar Santana <cjsseng@gmail.com>
2022-04-06 15:20:47 -0300

    up pic_carver

--- network/sniff/pic_carver.py
+++ network/sniff/pic_carver.py
@@ -8,6 +8,68 @@ pictures_directory = "home/user/Desktop/pictures"
 faces_directory = "/home/justin/pic_carver/faces"
 pcap_file = "bhp.pcap"
 
+def get_http_headers(http_payload):
+
+    try:
+        #separa os cabeçalhos se for tráfego HTTP
+        headers_raw = http_payload[:http_payload.index("\r\n\r\n")+2]
+
+        #divido os campos do cabeçalhos
+        headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", headers_raw))
+    except:
+        return None
+
+    if "Content-type" not in headers:
+        return None
+    
+    return headers
+
+def extract_image(headers, http_payload):
+
+    image = None
+    image_type = None
+
+    try:
+        if "image" in headers['Content-type']:
+
+            #obtem o tipo da imagem e o corpo
+            image_type = headers['Content-type'].split("/")[1]
+
+            image = http_payload[http_payload.index("\r\n\r\n")+4:]
+
+            #se uma compactação for detectada, descompactada a imagem
+            try:
+                if "Content-Encoding" in headers.keys():
+                    if headers['Content-Encoding'] == "gzip":
+                        image = zlib.decompress(image, 16+zlib.MAX_WBITS)
+                    elif headers['Content-Encoding'] == "deflate":
+                        image = zlib.decompress(image)
+            
+            except:
+                pass
+    except:
+        return None, None
+
+    return image,image_type
+
+def face_detect(path, file_name):
+
+    img = cv2.imread(path)
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CH_HAAR_SCALE_IMAGE, (20,20))
+
+    if len(rects) == 0:
+        return False
+    
+    rects[:, 2:] += rects[:, :2]
+
+    for x1,y1,x2,y2 in rects:
+        cv2.rectangle(img,(x1,y1),(x2,y2), (127,255,0),2)
+
+    cv2.imwrite("%s/%s-%s" % (faces_directory,pcap_file, file_name), img)
+    
+    return True
+
 def http_assembler(pcap_file):
     
     
