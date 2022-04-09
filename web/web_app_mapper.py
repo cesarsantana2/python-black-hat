0c52fdc488ff68334d212e60e71826843596fb5c
Cesar Santana <cjsseng@gmail.com>
2022-04-09 16:06:27 -0300

    adding web app mapper

--- web/web_app_mapper.py
+++ web/web_app_mapper.py
@@ -1,9 +1,45 @@
-import queue
+import Queue
 import threading
 import os
 import urllib3
 
 threads = 10
 
-target = "https://wwww.google.com.br"
+target = "https://www.wonderingschool.org/"
+directory = "/home/cs/Documents/map"
+filters = [".jpg", ".gif", ".png", ".css"]
 
+os.chdir(directory)
+
+web_paths = Queue.Queue()
+
+for r,d,f in os.wal("."):
+    for files in f:
+        remote_path = "%s/%s" % (r,files)
+        if remote_path.startswith("."):
+            remote_path = remote_path[1:]
+        if os.path.splitext(files)[1] not in filters:
+            web_paths.put(remote_path)
+
+def test_remote():
+    while not web_paths.empty():
+        path = web_paths.get()
+        url = "%s%s" % (target, path)
+
+        request = urllib3.Request(url)
+
+        try:
+            response = urllib3.urlopen(request)
+            content = response.read()
+
+            print("[%d] => %s" % (response.code, path))
+            response.close()
+
+        except urllib3.HTTPError as error:
+            #print("Failed %s" % error.code)
+            pass
+
+for i in range(threads):
+    print("Spawing thread: %d" % i)
+    t = threading.Thread(target=test_remote)
+    t.start()
