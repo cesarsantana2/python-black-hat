5491c089ffbff796090e310297a898d70e4d011a
Cesar Santana <cjsseng@gmail.com>
2022-04-16 13:52:26 -0300

    terminando o BHPFuzzer

--- web/burp/bhp_fuzzer.py
+++ web/burp/bhp_fuzzer.py
@@ -57,4 +57,33 @@ class BHPFuzzer(IIntruderPayloadGenerator):
         self.num_iterations = 0
         return 
 
-    
+
+    def mutate_payload(self, original_payload):
+        #escolhe um modificador simples ou pode até mesmo chamar um script exteno
+        picker = random.randint(1,3)
+
+        #seleciona um offset aleatório no payload para ser modificado 
+        offset = random.randint(0, len(original_payload)-1)
+        payload = original_payload[:offset]
+
+        #insere uma tentativa de injeção de SQL no offset aleatório
+        if picker == 1:
+            payload += "'"
+        
+        #insere uma tentativa de XSS
+        if picker == 2:
+            payload += "<script>alert('BHP!');</script>"
+
+        #repete uma porção do payload original uma quantidade aleatória de vezes 
+        if picker == 3:
+
+            chunk_length = random.randint(len(payload[offset:]),len(payload)-1)
+            repeater = random.randint(1,10)
+
+            for i in range(repeater):
+                payload += original_payload[offset:offset+chunk_length]
+
+        #acrescenta a parte restante do payload
+        payload += original_payload[offset:]
+
+        return payload
