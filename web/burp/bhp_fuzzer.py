3f21fc7682fe87eb646afe15bb8736be3e3d8fea
Cesar Santana <cjsseng@gmail.com>
2022-04-16 10:09:45 -0300

    wip

--- web/burp/bhp_fuzzer.py
+++ web/burp/bhp_fuzzer.py
@@ -1,3 +1,5 @@
+from configparser import ExtendedInterpolation
+from email.message import _PayloadType
 from burp import IBurpExtender
 from burp import IIntruderPayloadGeneratorFactory
 from burp import IIntruderPayloadGenerator
@@ -22,3 +24,37 @@ class BurpExtender(IBurpExtender, IIntruderPayloadGeneratorFactory):
         return BHPFuzzer(self, attack)
 
 
+class BHPFuzzer(IIntruderPayloadGenerator):
+    def __init__(self, extender, attack):
+        self._extender = extender
+        self._helpers = extender._helpers
+        self._attack = attack
+        self.max_payloads = 10
+        self.num_iterations = 0
+
+        return
+
+    def hasMorePayloads(self):
+        if self.num_iterations == self.max_payloads:
+            return False
+        else:
+            return True
+
+    def getNextPayload(self, current_payload):
+
+        #converte em uma string
+        payload = "".join(chr(x) for x in current_payload)
+
+        #chama o nosso modificador simples para fazer fuzzing no POST
+        payload = self.mutate_payload(payload)
+
+        #incrementa o numero de tentativas de fuzzing
+        self.num_iterations += 1
+
+        return payload
+
+    def reset(self):
+        self.num_iterations = 0
+        return 
+
+    
